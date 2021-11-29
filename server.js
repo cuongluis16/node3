6fbd845e73a2d0d47711d6f5205a8be729b7b435
cuongluis16 <200062pf@yse-c.net>
2021-11-29 11:42:36 +0900

    dfg

---
+++ server.js
@@ -0,0 +1,37 @@
+const express = require('express');
+
+require('dotenv').config();
+const host = process.env.HOST;
+const port = process.env.PORT;
+
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(__dirname + '/public'));
+
+app.get("/", (req, res) => {
+    console.log(req.body);
+    console.log(req.url);
+    console.log(req.query);
+
+    res.send("Hello YSE!");
+});
+
+app.get("/profile", (req, res) => {
+    res.send("This is Profile page.");
+});
+
+app.post('/auth', (req, res) => {
+    const login_name = req.body.login_name;
+    const password = req.body.password;
+
+    let message = 'ログインできませんでした';
+    if (login_name == process.env.LOGIN_NAME && password == process.env.PASSWORD) {
+        message = 'ログインしました';
+    }
+    res.send(message);
+})
+
+app.listen(port, host, () => {
+    console.log(`Server listen: http://${host}:${port}`);
+});
