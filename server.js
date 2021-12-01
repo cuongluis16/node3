aa75f2c2550b3c75f6ea1ccddc5faaba94754c1a
cuongluis16 <200062pf@yse-c.net>
2021-12-01 11:50:47 +0900

    GG

--- server.js
+++ server.js
@@ -1,37 +1,24 @@
-const express = require('express');
+const express = require('express')
+//routes.js をよみこむ
+const routes = require('./routes')
 
-require('dotenv').config();
-const host = process.env.HOST;
-const port = process.env.PORT;
+const dotenv = require('dotenv')
 
-const app = express();
+dotenv.config()
+const host = process.env.HOST
+const port = process.env.PORT
 
-app.use(express.urlencoded({ extended: true }));
-app.use(express.static(__dirname + '/public'));
+const app = express()
 
-app.get("/", (req, res) => {
-    console.log(req.body);
-    console.log(req.url);
-    console.log(req.query);
+app.use(express.urlencoded({ extended: true }))
+app.use(express.static(__dirname + '/public'))
 
-    res.send("Hello YSE!");
-});
+//ejs をつかう
+app.set('view engine', 'ejs')
 
-app.get("/profile", (req, res) => {
-    res.send("This is Profile page.");
-});
-
-app.post('/auth', (req, res) => {
-    const login_name = req.body.login_name;
-    const password = req.body.password;
-
-    let message = 'ログインできませんでした';
-    if (login_name == process.env.LOGIN_NAME && password == process.env.PASSWORD) {
-        message = 'ログインしました';
-    }
-    res.send(message);
-})
+//routes をつかう
+app.use(routes)
 
 app.listen(port, host, () => {
-    console.log(`Server listen: http://${host}:${port}`);
-});
+    console.log('http://' + host + ':' + port)
+})
